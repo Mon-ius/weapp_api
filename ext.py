9f301337a6e0823d319720a1d57d01a224fc22e7
monius <m1105930979@gmail.com>
2018-09-26 17:53:23 +0800

    fixed

--- ext.py
+++ ext.py
@@ -2,7 +2,7 @@ import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
-from flask_images import Images
+
 from flask_babel import Babel, _, lazy_gettext as _l
 from config import Config
 from flask_login import LoginManager
@@ -16,7 +16,7 @@ from flask_httpauth import HTTPBasicAuth
 db = SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
-images = Images()
+
 login = LoginManager()
 babel = Babel()
 auth = HTTPBasicAuth()
